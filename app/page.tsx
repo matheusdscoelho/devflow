3be6654c9d3f2ca32293e335cc159303d3d8fd3d
Matheus Coelho <matheuscoelho060@gmail.com>
2025-04-09 12:14:15 -0300

    Assets and MetaData

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <h1 className="h1-bold">Welcomedsfdsfdsfdfdsfsdfsdsdfsds home</h1>;
+  return (
+    <>
+      <h1 className="h1-bold text-4xl text-center text-gray-900 ">
+        Welcomedsfdsfdsfdfdsfsdfsdsdfsds home
+      </h1>
+      <h1 className="h1-bold font-space-grotesk text-4xl text-center text-gray-900 ">
+        Welcomedsfdsfdsfdfdsfsdfsdsdfsds home
+      </h1>
+    </>
+  );
 }
